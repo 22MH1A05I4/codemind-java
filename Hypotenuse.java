438b7bad6618d5e4bddbe1d32d0d59e1c2c52f32
22MH1A05I4 <119779629+22MH1A05I4@users.noreply.github.com>
2024-02-05 12:16:22 +0530

    import java.util.*; public class Hypotenuse{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         double c=Math.sqrt(Math.pow(a,2)+Math.pow(b,2));         System.out.printf("%.2f",c);
        }
    }

---
+++ Hypotenuse.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Hypotenuse{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        double c=Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
+        System.out.printf("%.2f",c);
+        
+    }
+}
